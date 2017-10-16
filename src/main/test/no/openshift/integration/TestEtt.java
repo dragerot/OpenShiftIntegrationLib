09f73e6f61f91d2f00b056fe45e9a3c7c3c30f2c
toregard <dragerot@gmail.com>
2017-10-16 10:34:56 +0200

    prlver

--- src/main/test/no/openshift/integration/TestEtt.java
+++ src/main/test/no/openshift/integration/TestEtt.java
@@ -1,20 +1,62 @@
 package no.openshift.integration;
 
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
-import no.openeshift.integration.OpenShiftConfig;
-import org.junit.Assert;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.jupiter.api.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@Import(OpenShiftConfig.class)
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayName("A special test case")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestEtt {
-    @Autowired
-    OpenShiftClient openShiftClient;
+    public static final String HOST_URL = "https://192.168.0.16:8443";
 
-    public void hallo(){
-        Assert.assertNotNull(openShiftClient);
+
+    OpenShiftClient target = null;
+
+    @BeforeAll
+    public void before() {
+        Config config = new ConfigBuilder()
+                .withMasterUrl(HOST_URL)
+                //.withOauthToken("ea4d0859-b1d7-11e7-bb08-8a91c37e8cd3").build();
+                .withUsername("developer")
+                .withPassword("developer").build();
+        DefaultOpenShiftClient defaultOpenShiftClient = new DefaultOpenShiftClient(config);
+       target = defaultOpenShiftClient.adapt(OpenShiftClient.class);
+
+    }
+
+    @AfterAll
+    public void after() {
+        target = null;
+    }
+
+    @Test
+    public void hallo() {
+        assertNotNull(target);
+        URL url = target.getOpenshiftUrl();
+        assertNotNull(url);
+        assertEquals(url.getPort(), 8443);
+
+        Namespace myns = target.namespaces().withName("detteprj").edit()
+                .editMetadata()
+                .addToLabels("a", "label")
+                .endMetadata()
+                .done();
+
+        //NamespaceList myNs = target.namespaces().list();
+
+//
+//        ServiceList myServices = target.services().list();
+//
+//        ServiceList myNsServices = target.services().inNamespace("default").list();
     }
 }
